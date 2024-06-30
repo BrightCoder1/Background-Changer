7a60f2199c060a65e479719db946fbb4192889ec
Vishal Kumar <vs035967@gmail.com>
2024-06-30 20:06:25 +0530

    bg1

---
+++ script.js
@@ -0,0 +1,11 @@
+let body = document.querySelector(".body");
+
+function red(){
+    document.body.style.background = "red"
+};
+function green(){
+    document.body.style.background = "green"
+};
+function yellow(){
+    document.body.style.background = "yellow"
+};
